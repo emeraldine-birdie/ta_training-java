347d0f313e0c4860c81acbd9865982e907794929
Ekaterina Moteva <sinkanight@mail.ru>
2022-03-21 19:01:38 +0300

    added changes

--- src/com/epam/training/student_Ekaterina_Moteva/fundamentals/main_task/HelloUser.java
+++ src/com/epam/training/student_Ekaterina_Moteva/fundamentals/main_task/HelloUser.java
@@ -1,7 +1,11 @@
 package com.epam.training.student_Ekaterina_Moteva.fundamentals.main_task;
 
+import java.math.BigDecimal;
+
 public class HelloUser {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args){
+        for (String user : args){
+            System.out.println("Hello, " + user + "!");
+        }
     }
 }
