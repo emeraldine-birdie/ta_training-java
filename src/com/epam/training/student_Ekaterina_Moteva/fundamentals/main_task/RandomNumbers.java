076aa87e14a3a10fcff96744f300a335f4634c8c
Ekaterina Moteva <sinkanight@mail.ru>
2022-03-21 19:19:04 +0300

    changes added

--- src/com/epam/training/student_Ekaterina_Moteva/fundamentals/main_task/RandomNumbers.java
+++ src/com/epam/training/student_Ekaterina_Moteva/fundamentals/main_task/RandomNumbers.java
@@ -2,6 +2,19 @@ package com.epam.training.student_Ekaterina_Moteva.fundamentals.main_task;
 
 public class RandomNumbers {
     public static void main(String[] args) {
-        
+        randomNumbersOnNewLine(5);
+        randomNumbersOnOneLine(5);
+    }
+
+    public static void randomNumbersOnNewLine(int count){
+        for (int i = 0; i < count; i++){
+            System.out.println(Math.random());
+        }
+    }
+
+    public static void randomNumbersOnOneLine(int count){
+        for (int i = 0; i < count; i++){
+            System.out.print(Math.random() + " ");
+        }
     }
 }
