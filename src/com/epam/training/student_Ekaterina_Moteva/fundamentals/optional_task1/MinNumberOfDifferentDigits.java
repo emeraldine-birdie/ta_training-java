90c7a8e66628356f48a50402e74e047db2fec809
Ekaterina Moteva <sinkanight@mail.ru>
2022-03-22 13:02:47 +0300

    changes added

---
+++ src/com/epam/training/student_Ekaterina_Moteva/fundamentals/optional_task1/MinNumberOfDifferentDigits.java
@@ -0,0 +1,7 @@
+package com.epam.training.student_Ekaterina_Moteva.fundamentals.optional_task1;
+
+public class MinNumberOfDifferentDigits {
+    public static void main(String[] args) {
+        
+    }
+}
